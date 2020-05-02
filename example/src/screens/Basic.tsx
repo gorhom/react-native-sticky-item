08decb02208b69ba10bdc1e77e8bcfbcac186276
Mo Gorhom <gorhom@me.com>
2020-05-02 23:09:01 +0200

    chore: updated examples styling

--- example/src/screens/Basic.tsx
+++ example/src/screens/Basic.tsx
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: '#CACACD',
   },
   text: {
--- example/src/screens/FacebookStoriesStyled.tsx
+++ example/src/screens/FacebookStoriesStyled.tsx
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: '#000',
   },
   text: {
