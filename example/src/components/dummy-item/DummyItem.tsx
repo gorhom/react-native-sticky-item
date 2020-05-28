269f84c2349b49e561b63b1c569e4a938c915f4e
Mo Gorhom <gorhom@me.com>
2020-05-28 11:43:42 +0200

    feat: handle initial index (#6)
    * chore: implement handling initialScrollIndex prop
    
    * chore: updated basic example

--- example/src/components/dummy-item/DummyItem.tsx
+++ example/src/components/dummy-item/DummyItem.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 interface DummyItemProps {
+  index: number;
   width: number;
   height: number;
   borderRadius: number;
@@ -9,12 +11,40 @@ interface DummyItemProps {
 }
 
 const DummyItem = ({
+  index,
   width,
   height,
   borderRadius,
   backgroundColor,
 }: DummyItemProps) => {
-  return <View style={{ width, height, borderRadius, backgroundColor }} />;
+  const containerStyle = useMemo(
+    () => [
+      styles.container,
+      {
+        width,
+        height,
+        borderRadius,
+        backgroundColor,
+      },
+    ],
+    [width, height, borderRadius, backgroundColor]
+  );
+  return (
+    <View style={containerStyle}>
+      <Text style={styles.text}>{index}</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
 
 export default DummyItem;
