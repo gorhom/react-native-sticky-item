aab0609ff89122ce86663a57fd9d7b4080708904
Mo Gorhom <gorhom@me.com>
2020-06-01 22:15:48 +0200

    chore: updated the sticky size calculation

--- src/components/sticky-item/StickyItem.tsx
+++ src/components/sticky-item/StickyItem.tsx
@@ -36,14 +36,12 @@ const StickyItem = ({
   isRTL,
 }: StickyItemProps) => {
   const containerRef = useRef<Animated.View>(null);
-  const threshold = itemWidth - stickyItemWidth - separatorSize;
+  const threshold = itemWidth - stickyItemWidth + separatorSize;
   //#region Container
   const animatedTranslateX = multiply(
     cond(
       greaterThan(x, threshold),
-      isRTL
-        ? SCREEN_WIDTH - stickyItemWidth - separatorSize * 2
-        : itemWidth - stickyItemWidth - separatorSize * 2,
+      isRTL ? SCREEN_WIDTH - stickyItemWidth : itemWidth - stickyItemWidth,
       isRTL
         ? add(x, SCREEN_WIDTH - itemWidth - separatorSize)
         : sub(x, separatorSize)
@@ -128,7 +126,7 @@ const StickyItem = ({
             if (container) {
               // @ts-ignore
               container.setNativeProps({
-                pointerEvents: 'auto',
+                pointerEvents: 'box-none',
               });
             }
           })
